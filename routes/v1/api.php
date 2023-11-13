150713368675314b69ad72c6e03a8970b17279ac
ahmad mohammadi <ahmadmohammadi940@gmail.com>
2023-11-13 22:51:27 +0330

    Add cache response

--- routes/v1/api.php
+++ routes/v1/api.php
@@ -4,6 +4,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::group([], function () {
     Route::prefix('articles')->group(function () {
-        Route::post('', [\App\Http\Controllers\Api\V1\ArticleController::class, 'index']);
+        Route::post('', [\App\Http\Controllers\Api\V1\ArticleController::class, 'index'])->middleware('cacheResponse:300');
     });
 });
